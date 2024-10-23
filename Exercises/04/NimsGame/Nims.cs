aae46cb50f3c6affa78dba61b574f3b2419b212a
xanderaerts <xanderaerts@gmail.com>
2024-10-23 18:10:22 +0200

    start nims game

---
+++ Exercises/04/NimsGame/Nims.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace NimsGame
+{
+    public class Nims
+    {
+        public int[] towers {get;set;}
+
+        public Nims(int[] towers){
+            this.towers[0] = towers[0];
+            this.towers[0] = towers[0];
+            this.towers[0] = towers[0];
+        }
+    }
+}
---
+++ Exercises/04/NimsGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+
+namespace NimsGame;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        int[] towers = new int[3];
+
+        for(int i = 0; i <3;i++ ){
+            towers[i] = Convert.ToInt32(input.Split(" ")[i]);
+        }
+
+        Nims nims = new Nims(towers);
+    }
+}
