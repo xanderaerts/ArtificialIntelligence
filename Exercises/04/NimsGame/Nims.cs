b02c0eb1f1525d32ae1de953813307b187af2288
xanderaerts <xanderaerts@gmail.com>
2024-11-03 20:41:03 +0100

    nims game

--- Exercises/04/NimsGame/Nims.cs
+++ Exercises/04/NimsGame/Nims.cs
@@ -1,16 +1,138 @@
 using System;
-using System.Runtime.InteropServices;
 
 namespace NimsGame
 {
     public class Nims
     {
         public int[] towers {get;set;}
+        public bool FysicalOponent {get;set;}
 
         public Nims(int[] towers){
-            this.towers[0] = towers[0];
-            this.towers[0] = towers[0];
-            this.towers[0] = towers[0];
+            this.towers = towers;
         }
+
+
+        public bool TowerEmpty(int tower){
+            if(this.towers[tower] == 0){
+                return false;
+            }
+ 
+            return true;            
+        }
+
+        public bool Wins(){
+            if(this.towers[0] == 0 && this.towers[1] == 0 && this.towers[2] == 0){
+               return true;
+            }
+            return false;
+        }
+
+        public void Move(int[] move){
+            if(move[0] > 3 || move[0] < 1){
+                throw new Exception("Invalid pile number.");
+            }
+            int newValue = this.towers[move[0] - 1] - move[1];
+
+            if(newValue < 0){
+                throw new Exception("Not enough items in the pile.");
+            }
+
+
+
+            this.towers[move[0] - 1] = newValue;
+        } 
+
+        public int[] Play(){
+            int bestScore = Int32.MinValue,bestValue = -1,bestTower = -1; //value represents the amount of items you take from the given tower
+            int towerCounter = 0;
+
+            foreach(int tower in this.towers){ // tower hosts the value of each tower
+                for(int i = 1; i <= tower;i++){
+                    int towervalue = tower;
+
+                    this.towers[towerCounter] -= i;
+                   // Console.WriteLine(this);
+
+                    int score = MinMax(false);
+
+                    this.towers[towerCounter] = towervalue;
+
+                    if(score > bestScore){
+                        bestScore = score;
+                        bestValue = i;
+                        bestTower = towerCounter;
+                    }
+
+                }
+                towerCounter++;
+            }
+
+            int[] bestMove = new int[2]{bestTower+1,bestValue};
+
+            return bestMove;
+        }
+
+        public int MinMax(bool isMax){
+            
+            if(Wins()){
+                if(isMax) return -1;
+                else return 1;
+            }
+
+            if(isMax){
+                int towerCounter = 0,bestScore = int.MinValue;
+
+                foreach(int tower in this.towers){
+                    for(int i = 1; i <= tower; i++){
+                        int towervalue = tower;
+
+                        this.towers[towerCounter] -= i;
+                        
+                        int score = MinMax(false);
+
+                        this.towers[towerCounter] = towervalue;
+
+                        bestScore = Math.Max(score,bestScore);
+                            
+                    }
+                    towerCounter++;
+                }
+
+                return bestScore;
+            }
+            else{
+                int bestScore = Int32.MaxValue, towerCounter = 0;
+
+                foreach(int tower in this.towers){
+                    for(int i = 1; i <= tower; i++){
+                        int towervalue = tower;
+
+                        this.towers[towerCounter] -= i;
+                        
+                        int score = MinMax(true);
+
+                        this.towers[towerCounter] = towervalue;
+
+                        bestScore = Math.Min(score,bestScore);
+                            
+                    }
+                    towerCounter++;
+                }
+
+                return bestScore;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string s = "";
+
+            for(int i = 0; i < this.towers.Length;i++){
+                s += this.towers[i] + " | ";
+            }
+            return s;
+        }
+
     }
 }
--- Exercises/04/NimsGame/Program.cs
+++ Exercises/04/NimsGame/Program.cs
@@ -1,18 +1,92 @@
-﻿using Microsoft.VisualBasic;
+﻿using System;
 
-namespace NimsGame;
-class Program
+namespace NimsGame{
+class Programs
 {
     static void Main(string[] args)
     {
-        string input = Console.ReadLine();
 
-        int[] towers = new int[3];
+        string inputTowers = "",inputPlayer = "";
+        int [] towers = new int[3];
+        try{
+            inputTowers = Console.ReadLine().Trim();
 
-        for(int i = 0; i <3;i++ ){
-            towers[i] = Convert.ToInt32(input.Split(" ")[i]);
+            inputPlayer = Console.ReadLine().Trim();
+
+            towers = new int[3];
+
+            for(int i = 0; i <3;i++ ){
+                towers[i] = Convert.ToInt32(inputTowers.Split(" ")[i]);
+            }
+
+        }
+        catch{
+            throw new Exception("Crazy input");
         }
 
-        Nims nims = new Nims(towers);
+        try{
+            int[] move = new int[2];        
+
+
+            char player = 'A'; //A = starter
+
+
+            Nims nims = new Nims(towers);
+
+            if(inputPlayer == "human") nims.FysicalOponent = true;
+            else if(inputPlayer == "computer") nims.FysicalOponent = false;
+            else throw new Exception("Crazy input!");
+
+            while(!nims.Wins()){  
+                //Console.WriteLine(nims);
+                
+
+                //Console.WriteLine(player);
+
+                if(player == 'A'){
+                    try{
+                        string[] s = Console.ReadLine().Trim().Split(" ");
+
+                        for(int i = 0; i < s.Length; i++){
+                            move[i] = Convert.ToInt32(s[i]);
+                        }
+                    }
+                    catch{
+                        throw new Exception("Crazy input!"); 
+                    }
+                }
+                else if(player == 'B' && nims.FysicalOponent == true){
+                    try{                   
+                        string[] s = Console.ReadLine().Trim().Split(" "); 
+
+                        for(int i = 0; i < s.Length; i++){
+                            move[i] = Convert.ToInt32(s[i]);
+                        }
+                    }
+                    catch{
+                        throw new Exception("Crazy input!"); 
+                    }
+                }
+                else{
+                    move = nims.Play();
+                    Console.WriteLine(string.Join(" ",move));
+                }
+
+
+                nims.Move(move);
+
+                if(nims.Wins()){
+                    Console.WriteLine($"{player} wins!");
+                    break;
+                }
+
+                if(player == 'A') player = 'B';
+                else player = 'A';
+            }
     }
+        catch(Exception e){
+            Console.WriteLine(e.Message);
+        }
+    }
+}
 }
